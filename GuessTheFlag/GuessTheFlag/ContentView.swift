75d88a490e32ac9bb4b576b9b40d2799185bc5a2
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-10 12:36:37 +0200

    [Project-2](Gradients): Background color gradient

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(
-            colors: [.red, .orange, .yellow, .green, .cyan, .blue, .indigo, .purple, .pink],
-            center: .center
-        )
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.black.gradient)
     }
 }
 
