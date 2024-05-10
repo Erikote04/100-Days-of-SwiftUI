8caa7372f8d79cff52764088a2640dcaa0017877
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-10 12:24:50 +0200

    [Project-2](Gradients): Linear gradient with stops

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         LinearGradient(
-            colors: [.white, .black],
+            stops: [
+                .init(color: .white, location: 0.45),
+                .init(color: .black, location: 0.55)],
             startPoint: .top,
             endPoint: .bottom
         )
