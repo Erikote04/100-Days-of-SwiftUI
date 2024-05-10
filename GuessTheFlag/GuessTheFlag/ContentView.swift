db33f385eba59cf10a79f45d895d4c933d33df26
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-10 12:47:00 +0200

    [Project-2](Buttons): Roles

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,14 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    func executeDelete() {
-        print("Now deleting...")
-    }
-    
     var body: some View {
-        Button("Delete selection") {
-            executeDelete()
+        VStack {
+            Button("Cancel", role: .cancel, action: {})
+            Button("Delete", role: .destructive, action: {})
         }
     }
 }
