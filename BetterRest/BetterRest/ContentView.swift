d64feadb8957047bd772aa0ee10f35238a84f8cd
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-14 19:24:25 +0200

    [Project-4]: Cleaning up the user interface

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -2,45 +2,58 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeup = Date.now
+    @State private var wakeup = defaultWakeTime
     @State private var  sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker(
+                        "Please, enter a time",
+                        selection: $wakeup,
+                        displayedComponents: .hourAndMinute
+                    ).labelsHidden()
+                }
                 
-                DatePicker(
-                    "Please, enter a time",
-                    selection: $wakeup,
-                    displayedComponents: .hourAndMinute
-                ).labelsHidden()
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(
+                        "\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25
+                    )
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper(
-                    "\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25
-                )
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(
-                    "\(coffeeAmount) cup(s)",
-                    value: $coffeeAmount,
-                    in: 1...20,
-                    step: 1
-                )
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(
+                        "^[\(coffeeAmount) cup](inflect: true)",
+                        value: $coffeeAmount,
+                        in: 0...20,
+                        step: 1
+                    )
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar {
