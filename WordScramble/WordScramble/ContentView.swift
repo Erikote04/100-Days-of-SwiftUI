ac5cb862c042a264f5f58eff2da6279cac98bd68
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-16 10:18:42 +0200

    [Project-5]: List, your new best friend

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -1,14 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(people, id: \.self) {
+            Text($0)
         }
-        .padding()
     }
 }
 
