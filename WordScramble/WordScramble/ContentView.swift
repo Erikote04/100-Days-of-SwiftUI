7353c6219012bb058c0441a82616254729f5664e
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-16 10:33:57 +0200

    [Project-5]: Working with strings

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -10,12 +10,24 @@ struct ContentView: View {
         }
     }
     
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string
-            }
-        }
+    func testStrings() {
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en"
+        )
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
