2964d49dbca935681a827f6ba06c02c3dd0eca34
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-16 20:42:00 +0200

    [Project-6]: Ease in out animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Bounce") {
+        Button("In Out") {
             animationAmount += 1
         }
         .padding(50)
@@ -12,7 +12,7 @@ struct ContentView: View {
         .foregroundStyle(.white)
         .clipShape(.circle)
         .scaleEffect(animationAmount)
-        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+        .animation(.easeInOut(duration: 2), value: animationAmount)
     }
 }
 
