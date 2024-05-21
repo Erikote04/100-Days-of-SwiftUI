7abb660cb40fe1ba1eac462fd1e89bcfb924e911
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-21 12:34:07 +0200

    [Project-8]: Resizing images to fit the available space

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -2,13 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(.example)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 200, height: 200)
+        
+        Image(.example)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+        
+        Image(.example)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal) { size, axis in
+                size * 0.8
+            }
     }
 }
 
