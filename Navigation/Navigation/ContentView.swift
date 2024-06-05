a84c4c3592a4bed957b962c451c9bd045f7f7da7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-06-05 11:02:20 +0200

    [Project-9]: How to make a NavigationStack return to its root view programmatically

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,35 +1,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path = [Int]()
     
     var body: some View {
         NavigationStack(path: $path) {
-            List{
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { number in
+                    DetailView(number: number, path: $path)
                 }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .toolbar {
-                Button("Push 556") {
-                    path.append(556)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { number in
-                Text("You selected the number \(number)")
-            }
-            .navigationDestination(for: String.self) { string in
-                Text("You selected the string \(string)")
-            }
         }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: [Int]
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path.removeAll()
+                }
+            }
     }
 }
 
