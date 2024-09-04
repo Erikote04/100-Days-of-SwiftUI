92712d9fd1ab66553c739a63a86927038b23d4d0
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-04 10:41:42 +0200

    [Project-1]: Creating a form

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -1,21 +1,28 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Erik Sebastian de Erice Jerez on 4/9/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        Form {
+            Section {
+                Text("Hello, world!")
+            }
+            
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
