503a425b47d625c4487545b878fb8ef4ac202161
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-04 09:33:00 +0200

    [Project-1]: Setting up

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Erik Sebastian de Erice Jerez on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
