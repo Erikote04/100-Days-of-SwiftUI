c14269079855eea5017d27bab7e66f042eca8cde
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-09 10:25:21 +0200

    [Project-2]: Showing alert messages

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,43 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingAlert = false
+    
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-            
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-            
-            Spacer()
-            
-            Image(systemName: "pencil.circle")
-                .foregroundStyle(.red)
-                .font(.largeTitle)
-            
-            Spacer()
-            
-            Button("Edit", systemImage: "pencil.circle") {
-                print("Button was tapped")
-            }
-            
-            Spacer()
-            
-            Button() {
-                print("Button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil.circle")
-            }
-            
-            Spacer()
+        Button("Show alert") {
+            isShowingAlert = true
+        }
+        .padding()
+        .alert("Important message", isPresented: $isShowingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this")
         }
     }
 }
