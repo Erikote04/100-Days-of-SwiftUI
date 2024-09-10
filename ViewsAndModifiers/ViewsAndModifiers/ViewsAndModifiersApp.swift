e3e400e8d5f4dbe2a9d13d89ecf07f44ef60ab9b
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-10 18:57:43 +0200

    [Project-3]: Setting up

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Erik Sebastian de Erice Jerez on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
