df9123ad32ef78514b36947774fae8189d551641
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-12 18:41:15 +0200

    [Project-3]: Custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -2,25 +2,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            CapsuleText(text: "First")
-                .foregroundStyle(.white)
+        Text("Hello, world!")
+            .titleStyle()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermark(text: "hackingwithswift.com")
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
             
-            CapsuleText(text: "Second")
-                .foregroundStyle(.yellow)
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
         }
     }
 }
 
-struct CapsuleText: View {
-    var text: String
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
     
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .background(.blue)
-            .clipShape(.capsule)
+    func watermark(text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
