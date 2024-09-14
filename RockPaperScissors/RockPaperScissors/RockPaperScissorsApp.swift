92bca559750b477eb70de527f9c006245102b530
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-14 23:44:54 +0200

    [Milestone](Projects 1-3): Setting up

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Erik Sebastian de Erice Jerez on 14/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
