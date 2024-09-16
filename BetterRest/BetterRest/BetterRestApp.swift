eac5f996e933008b745fd2514b1749e70551a49c
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-16 22:26:58 +0200

    [Project-4]: Setting up

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
