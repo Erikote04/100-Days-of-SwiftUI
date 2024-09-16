9755c8e20d55b676b3d59c5214c83719ad6fe5c4
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-16 23:27:44 +0200

    [Project-4]: Building a basic layout

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -1,21 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeup = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text(Date.now, format: .dateTime.hour().minute())
-        Text(Date.now, format: .dateTime.year().month().day())
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeup, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
     }
     
-    func exampleDates() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
+    func calculateBedTime() {
         
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
     }
 }
 
