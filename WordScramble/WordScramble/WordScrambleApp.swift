237d91c46a31eb172378c46f06e881c63020acc5
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-18 21:13:54 +0200

    [Project-5]: Setting up

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Erik Sebastian de Erice Jerez on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
