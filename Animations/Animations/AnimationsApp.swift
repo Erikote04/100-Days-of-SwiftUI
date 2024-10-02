2595baa851763f7a4766de29f0f82717c9fb98b7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-02 22:16:14 +0200

    [Project-6]: Setting up

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Erik Sebastian de Erice Jerez on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
