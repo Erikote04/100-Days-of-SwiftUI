56adc658d3d813a98685f557a9187ff68eb6420a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-07 16:04:51 +0200

    [Milestone](Projects-4-6): Setting up

---
+++ MathQuest/MathQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathQuest
+//
+//  Created by Erik Sebastian de Erice Jerez on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathQuest/MathQuest/MathQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuestApp.swift
+//  MathQuest
+//
+//  Created by Erik Sebastian de Erice Jerez on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MathQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MathQuestView()
+        }
+    }
+}
