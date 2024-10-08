91dc173211be4d8a45d77451006af17d3c16c0ba
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-08 18:49:13 +0200

    [Project-7]: Setting up

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Erik Sebastian de Erice Jerez on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
