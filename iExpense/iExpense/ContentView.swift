0790cd56a26726df4ebacc4ef9325ce14d31fe74
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-10 01:02:54 +0200

    [Project-7]: Working with Identifiable items in SwiftUI

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
@@ -26,7 +26,8 @@ struct ContentView: View {
     }
 }
 
-struct ExpenseItem: Codable {
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
