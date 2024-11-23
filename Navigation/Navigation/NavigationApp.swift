8b827b6f86472132dbcfb4fc0126964af35cf3cb
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-11-23 19:13:31 +0100

    [Project-9]: Setting Up

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Erik Sebastian de Erice Jerez on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
