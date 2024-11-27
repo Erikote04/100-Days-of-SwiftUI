dc82d6bbba8f4b15afa5fabff96789b4f8516d9e
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-11-27 21:57:17 +0100

    [Project-9]: Placing toolbar buttons in exact locations

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -10,13 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) { i in
-                Text("Row \(i)")
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue)
-            .toolbarColorScheme(.dark)
+            Text("Hello World")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Tap me") { }
+                    }
+                }
         }
     }
 }
