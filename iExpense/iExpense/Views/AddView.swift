04ec2aa631a3173072dd60b1c4a2dfeba8fc3c1d
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-12-03 18:26:42 +0100

    [Project-9](Challenge-2): Letting the user edit their issue name in navigation bar

--- iExpense/iExpense/Views/AddView.swift
+++ iExpense/iExpense/Views/AddView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+    @State private var name = "Expense name"
     @State private var type: ExpenseType = .personal
     @State private var amount = 0.0
     
@@ -19,8 +19,6 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                
                 Picker("Type", selection: $type) {
                     ForEach(ExpenseType.allCases, id: \.self) { type in
                         Text(type.rawValue)
@@ -30,7 +28,7 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add Expense")
+            .navigationTitle($name)
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 Button("Save") {
